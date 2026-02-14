111f93dc8fe9b7a16f2ccf5046f24af0cfdd12d1
alvaroq37 <alvaro_q23@hotmail.com>
2026-02-14 14:30:34 -0400

    # Se agregan los componentes para clientes

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <div>
+    <div>
+      <RouterLink
+        :to="{ name: 'agregar-cliente' }"
+        class="block md:inline-block rounded bg-blue-600 py-2 px-3"
+        >Agregar Cliente</RouterLink
+      >
+    </div>
+    <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
+  </div>
+</template>
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <div>
+    <RouterLink :to="{ name: 'inicio' }" class="ml-4 text-blue-500 hover:underline"
+      >volver</RouterLink
+    >
+    <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
+  </div>
+</template>
