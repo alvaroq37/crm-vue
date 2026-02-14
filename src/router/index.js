4ef9bcbabee10db987d9af34b85330f672e8e742
alvaroq37 <alvaro_q23@hotmail.com>
2026-02-14 14:30:08 -0400

    # Se configuran las rutas necesarias

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Inicio from "../views/ClientesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/",
+      name: "inicio",
+      component: Inicio,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/",
+      name: "agregar-cliente",
+      component: () => import("../views/NuevoClienteView.vue"),
     },
   ],
-})
+});
 
-export default router
+export default router;
